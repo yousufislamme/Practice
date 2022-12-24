013b9a02c46ea4ecba92fc4c5aab6dd6548aae83
yousufislamme <online.okk@gmail.com>
2022-12-24 21:20:37 +0600

    add a useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 function App() {
+  const [name, setName] = useState("Yousuf")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My name is: {name}</h1>
+
+      <button type="button" onClick={() => setName("Ibrahim")}>Ibrahim</button>
+      <button type="button" onClick={() => setName("Anas")}>Anas</button>
+      <button type="button" onClick={() => setName("Aouf")}>Aouf</button>
     </div>
   );
 }
